4decd31be8b0c269a7c5758a3a1ba2ef48ed7d5b
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-12-28 22:21:02 +0100

    Created basic play action

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,6 +2,11 @@ module Api
   module V1
     class BaseController < RocketPants::Base
       def play
+        filename = `youtube-dl #{params[:url]} --get-id`.strip + '.mp3'
+        `youtube-dl -x --id --audio-format mp3 #{params[:url]}` unless File.file?(filename)
+        `killall #{APP_CONFIG['audio_player']}`
+        fork { `#{APP_CONFIG['audio_player']} #{filename}` }
+        head :ok
       end
     end
   end
