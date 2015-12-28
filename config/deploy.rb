1a8cea60c144a21197f3f664507c7e021bfede65
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-12-28 19:58:05 +0100

    Removed asset_precompile from deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,6 @@ task deploy: :environment do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
-    invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     to :launch do
